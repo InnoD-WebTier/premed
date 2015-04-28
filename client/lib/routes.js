4c367c7b949da44eff47acea7152bfb3e5b56fa6
Magdalene Lim <qylim@berkeley.edu>
2015-04-27 17:02:18 -0700

    made landing page

--- client/lib/routes.js
+++ client/lib/routes.js
@@ -6,6 +6,10 @@ Router.route('/suggestion', function() {
 	this.render('suggestion');
 });
 
+Router.route('/landing', function() {
+	this.render('landing');
+});
+
 Router.route('/suggestionListView', function() {
 	this.render('suggestionListView');
 });
