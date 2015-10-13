334ce0e6f3267d90248f844422c1c72c3b4cc083
Arlan Jaska <ajaska@berkeley.edu>
2015-10-12 19:52:17 -0700

    Only set 404 title on 404 pages

--- client/views/404.js
+++ client/views/404.js
@@ -1,1 +1,4 @@
-document.title = '404 - Page not found'
+// Have to use Template['404'] instead of Template.404
+Template['404'].onCreated(function(){
+  document.title = '404 - Page not found';
+});
