b4c01b39bcda126dcba33c5b21b0fcbde24f5b2a
Julia Sun <julia.sun@berkeley.edu>
2015-10-27 13:44:10 -0700

    Finished advising page

---
+++ client/views/advising.js
@@ -0,0 +1,60 @@
+Template.advising.helpers({
+    dept: [
+        {
+            header: 'Molecular and Cell Biology',
+            general: 'https://mcb.berkeley.edu/undergrad/advising/advising-office/advising-services',
+            peer: 'http://mcb.berkeley.edu/undergrad/advising/advising-office/peer-advising/',
+            title: 'MCB Peer Advising (PAWS)',
+            email: 'mcbpaws@gmail.com',
+            phone: '(510) 643-8895',
+            location: '3060 VLSB',
+            description: 'Peer advisors are junior & senior MCB majors who volunteer their time to complement the UAO advising services by sharing their knowledge of and experience with lower division requirements and upper division classes, experience with student groups on campus, preparation for life beyond the B.A., and use of various campus resources. During the semester, you can visit peer advisors in the MCB UAO (3060 VLSB) or via email or gchat'
+        },
+        {
+            header: 'Integrative Biology',
+            general: 'https://ib.berkeley.edu/undergrad/advising.php',
+            peer: 'https://ib.berkeley.edu/undergrad/peer/peer_advising.php',
+            title: 'IB Peer Advising',
+            email: 'IBPeerAdvising@gmail.com',
+            phone: '(510) 643-1667',
+            location: '3060 VLSB',
+            description: 'Our Peer Advising Program provides an excellent service to IB students, complementing the advising of the faculty and staff. Our peer advisors\n- advise undeclared students\n- help with choosing a track in the major\n- share firsthand knowledge of course demands\n- make suggestions on course combinations\n- provide general information about applying to medical, pre-health professional and graduate schools\n- are a great resouce for information about many other student experiences'
+        },
+        {
+            header: 'Bioengineering',
+            general: 'http://coe.berkeley.edu/students/current-undergraduates/advising-and-tutoring.html',
+            peer: 'http://coe.berkeley.edu/students/current-undergraduates/advising/peer-advising-program/peer-advising-program.html',
+            title: 'Bio Engineering Peer Advising',
+            email: 'ESSpeeradvisers@coe.berkeley.edu',
+            phone: '(510) 642-7594',
+            location: '230 Bechtel Engineering Center',
+            description: 'Sometimes it\'s helpful to have an experienced student\'s perspective on how to juggle classes, study, manage time, choose the best enrichment opportunities, or how to navigate the waters of a large university like Cal. In cases like this, ESS Peer Advisers are an amazing resource! They can also assist in answering questions like how to drop a class or choose an elective, and can tell you all about life in their particular major, (and COE in general!). ESS Peer Advisers provide general information to engineering undergraduates regarding university and college requirements and procedures. This includes information about registration, deadlines, research and leadership opportunities, student organizations, campus resources and special events. Peer Advisers also meet with prospective students and lead workshops throughout the semester.'
+        }
+    ],
+    advisors: [
+        {
+            department: 'Molecular Environmental Biology',
+            advisor: 'Elizabeth Storer',
+            email: 'estorer@berkeley.edu',
+            phone: '(510) 642-1986'
+        },
+        {
+            department: 'Microbial Biology & Conservation and Resource Studies',
+            advisor: 'Ricky Vides',
+            email: 'rickyv72@berkeley.edu',
+            phone: '(510) 642-6730'
+        },
+        {
+            department: 'Nutritional Science and Toxicology',
+            advisor: 'Nicole Lowy',
+            email: 'nlowy@berkeley.edu',
+            phone: '(510) 642-2879'
+        },
+        {
+            department: 'Genetics and Plant Biology',
+            advisor: 'Jennifer Halpert',
+            email: 'jhalpert@berkeley.edu',
+            phone: '(510) 642-4249'
+        }
+    ]
+});
